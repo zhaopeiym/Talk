1734e308b32a06e13f906bcc1b70f2b9ed07e5a9
benny <zhaopeiym@163.com>
2018-08-31 22:46:43 +0800

    nuget发布

---
+++ Talk/自增版本号打包.ps1
@@ -0,0 +1,18 @@
+$LibraryName = "Talk"
+[xml]$versionXml = Get-Content ./version.props
+$Version = $versionXml.Project.PropertyGroup.VersionPrefix.split("\.")
+$Version[3] = ($version[3] -as [int]) + 1 
+$Version = $Version -join "."
+$versionXml.Project.PropertyGroup.VersionPrefix = $Version
+$versionSuffix = $versionXml.Project.PropertyGroup.VersionSuffix
+$versionXml.Save("$PSScriptRoot/version.props")
+
+# 打包
+Write-Host '开始打包...'
+dotnet pack /p:Version=$Version$versionSuffix -c Release
+
+Write-Host '开始上传...'
+nuget push ./bin/Release/$LibraryName.$Version$versionSuffix.nupkg -ApiKey oy2irwyzh6oprjroljwav5aospph6tuepbjibnvdcwmhq4 -Source https://api.nuget.org/v3/index.json
+
+Write-Host '输入空格退出...' -NoNewline
+$null = [Console]::ReadKey('?') #等待输入按键
