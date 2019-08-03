70f470978adb9a74c5d36388f4739bf9ceba40cc
benny <zhaopeiym@163.com>
2019-08-03 17:12:33 +0800

    fix

---
+++ Talk/Application/Models/RemoteAttestationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Talk.Application.Models
+{
+    /// <summary>
+    /// 远程认证
+    /// </summary>
+    public class RemoteAttestationModel
+    {
+        /// <summary>
+        /// 边缘设备表示
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 租户Id
+        /// </summary>
+        public long TenantId { get; set; }
+        /// <summary>
+        /// 项目Id
+        /// </summary>
+        public long ProjectId { get; set; }
+        /// <summary>
+        /// 认证加密键
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
