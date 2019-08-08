52b0312d9d58aba96ff91d65b8e290ae300e576e
benny <zhaopeiym@163.com>
2019-08-08 09:24:48 +0800

    fix

---
+++ Talk.Tests/Temp_Tests.cs
@@ -0,0 +1,55 @@
+﻿using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Core;
+using Com.Ctrip.Framework.Apollo.Enums;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Talk.Tests
+{
+    public class Temp_Tests
+    {
+        [Fact]
+        public void test()
+        {
+            var options = new ConfigurationBuilder()
+                //.AddInMemoryCollection(new Dictionary<string, string>
+                //{
+                //    {"Apollo:AppId", "apollo-client" },
+                //    {"Apollo:Env", "Pro" },
+                //})
+                .Build()
+                .GetSection("Apollo")
+                .Get<ApolloOptions>();
+            //string someKey = "someKeyFromDefaultNamespace";
+            //string someDefaultValue = "someDefaultValueForTheKey";
+            //string value = config.GetProperty(someKey, someDefaultValue);
+            Dictionary<string, string> Meta = new Dictionary<string, string>();
+            Meta.Add("DEV", "url");
+
+            var builder = new ConfigurationBuilder().AddApollo(new ApolloOptions
+            {
+                AppId = "apollo-client",
+                MetaServer = "url",
+                Env = Env.Dev,
+                Meta = Meta,
+            });
+            builder.AddDefault();
+            builder.AddNamespace("TEST1.Benny");
+            builder.AddNamespace(ConfigConsts.NamespaceApplication);
+            var bu = builder.Build();
+
+            for (int i = 0; i < 500; i++)
+            {
+                var aa = bu.GetValue<string>("test");
+            }
+
+            var sources = builder.Sources.OfType<ApolloConfigurationProvider>().ToArray();
+
+            var obj = builder.ConfigRepositoryFactory.GetConfigRepository(ConfigConsts.NamespaceApplication);
+            var config = obj.GetConfig();
+            var tem = config.GetProperty("test");
+        }
+    }
+}
