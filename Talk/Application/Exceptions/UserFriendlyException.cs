6b4c31c2030bfd4273ea72fd5aee72c84a817855
benny <zhaopeiym@163.com>
2019-08-10 09:45:55 +0800

    fix

---
+++ Talk/Application/Exceptions/UserFriendlyException.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Talk.Enums;
+
+namespace Talk.Application.Exceptions
+{
+    /// <summary>
+    /// 自定义友好异常
+    /// </summary>
+    public class UserFriendlyException : Exception
+    {
+        /// <summary>
+        /// 异常的附加信息
+        /// </summary>
+        public string Details { get; private set; }
+
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        public ExceptionCodeEnum Code { get; set; } = ExceptionCodeEnum.C500;
+         
+        public UserFriendlyException()
+        {
+        }
+
+        public UserFriendlyException(string message)
+          : base(message)
+        {
+
+        }
+
+        public UserFriendlyException(ExceptionCodeEnum code, string message)
+          : this(message)
+        {
+            Code = code; 
+        }
+
+        public UserFriendlyException(string message, string details)
+           : this(message)
+        {
+            Details = details; 
+        }
+
+        public UserFriendlyException(ExceptionCodeEnum code, string message, string details)
+          : this(message, details)
+        {
+            Code = code; 
+        }
+
+        public UserFriendlyException(string message, Exception innerException)
+           : base(message, innerException)
+        {
+
+        }
+
+        public UserFriendlyException(string message, string details, Exception innerException)
+            : this(message, innerException)
+        {
+            Details = details; 
+        }
+    }
+}
---
+++ Talk/Enums/ExceptionCodeEnum.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Talk.Enums
+{
+    public enum ExceptionCodeEnum
+    {
+        [Description("其他")]
+        Other = 200,
+        /// <summary>
+        /// 未登录
+        /// </summary>
+        [Description("未登录")]
+        C401 = 401,
+        /// <summary>
+        /// 没权限
+        /// </summary>
+        [Description("没权限")]
+        C407 = 407,
+        /// <summary>
+        /// 参数验证失败
+        /// </summary>
+        [Description("参数验证失败")]
+        C412 = 412,
+        /// <summary>
+        /// 内部错误
+        /// </summary>
+        [Description("内部错误")]
+        C500 = 500,
+        /// <summary>
+        /// 服务不可用
+        /// </summary>
+        [Description("服务不可用")]
+        C503 = 503,
+        /// <summary>
+        /// 请求超时
+        /// </summary>
+        [Description("请求超时")]
+        C504 = 504,
+    }
+}
